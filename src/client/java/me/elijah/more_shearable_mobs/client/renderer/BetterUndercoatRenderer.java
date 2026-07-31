c5345f021de9c8fb6a308a6ac37da1ec6d2c8aa9
vampad <artem-ch@outlook.com>
2026-07-30 18:18:35 -0600

    Port mod to Minecraft 26.1.2

--- src/client/java/me/elijah/more_shearable_mobs/client/renderer/BetterUndercoatRenderer.java
+++ src/client/java/me/elijah/more_shearable_mobs/client/renderer/BetterUndercoatRenderer.java
@@ -9,45 +9,46 @@ package me.elijah.more_shearable_mobs.client.renderer;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.command.OrderedRenderCommandQueue;
-import net.minecraft.client.render.entity.feature.FeatureRenderer;
-import net.minecraft.client.render.entity.feature.FeatureRendererContext;
-import net.minecraft.client.render.entity.model.EntityModel;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.LoadedEntityModels;
-import net.minecraft.client.render.entity.model.SheepEntityModel;
-import net.minecraft.client.render.entity.model.SheepWoolEntityModel;
-import net.minecraft.client.render.entity.state.SheepEntityRenderState;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.entity.passive.SheepEntity;
-import net.minecraft.util.DyeColor;
-import net.minecraft.util.Identifier;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
+import net.minecraft.client.renderer.entity.RenderLayerParent;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.model.geom.ModelLayers;
+import net.minecraft.client.model.geom.EntityModelSet;
+import net.minecraft.client.model.animal.sheep.SheepModel;
+import net.minecraft.client.model.animal.sheep.SheepFurModel;
+import net.minecraft.client.renderer.entity.state.SheepRenderState;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.resources.Identifier;
 
 import static me.elijah.more_shearable_mobs.ShearDataTrackers.IS_SHEEP_BUTCHERED;
 
 @Environment(EnvType.CLIENT)
-public class BetterUndercoatRenderer extends FeatureRenderer<SheepEntityRenderState, SheepEntityModel> {
-    private static final Identifier TEXTURE = Identifier.ofVanilla("textures/entity/sheep/sheep_wool_undercoat.png");
-    private final EntityModel<SheepEntityRenderState> model;
-    private final EntityModel<SheepEntityRenderState> babyModel;
+public class BetterUndercoatRenderer extends RenderLayer<SheepRenderState, SheepModel> {
+    private static final Identifier TEXTURE = Identifier.withDefaultNamespace("textures/entity/sheep/sheep_wool_undercoat.png");
+    private final EntityModel<SheepRenderState> model;
+    private final EntityModel<SheepRenderState> babyModel;
 
-    public BetterUndercoatRenderer(FeatureRendererContext<SheepEntityRenderState, SheepEntityModel> context, LoadedEntityModels loader) {
+    public BetterUndercoatRenderer(RenderLayerParent<SheepRenderState, SheepModel> context, EntityModelSet loader) {
         super(context);
-        this.model = new SheepWoolEntityModel(loader.getModelPart(EntityModelLayers.SHEEP_WOOL_UNDERCOAT));
-        this.babyModel = new SheepWoolEntityModel(loader.getModelPart(EntityModelLayers.SHEEP_BABY_WOOL_UNDERCOAT));
+        this.model = new SheepFurModel(loader.bakeLayer(ModelLayers.SHEEP_WOOL_UNDERCOAT));
+        this.babyModel = new SheepFurModel(loader.bakeLayer(ModelLayers.SHEEP_BABY_WOOL));
     }
 
     @Override
-    public void render(MatrixStack matrices, OrderedRenderCommandQueue queue, int light, SheepEntityRenderState state, float limbAngle, float limbDistance) {
-        SheepEntity sheep = ((ShearableSheepEntityRenderer.ShearableSheepRenderState) state).sheepEntity;
+    public void submit(PoseStack matrices, SubmitNodeCollector queue, int light, SheepRenderState state, float limbAngle, float limbDistance) {
+        ShearableSheepEntityRenderer.ShearableSheepRenderState shearableState =
+                (ShearableSheepEntityRenderer.ShearableSheepRenderState) state;
 
-        if (sheep.getDataTracker().get(IS_SHEEP_BUTCHERED)) return;
+        if (shearableState.isButchered) {
+            return;
+        }
 
         //TODO: test having a sheep named Jeb_
-        if (!state.invisible && (state.rainbow || state.color != DyeColor.WHITE)) {
-            EntityModel<SheepEntityRenderState> entityModel = state.baby ? this.babyModel : this.model;
-            render(entityModel, TEXTURE, matrices, queue, light, state, state.getRgbColor(), 0);
+        if (!state.isInvisible && (state.isJebSheep || state.woolColor != DyeColor.WHITE)) {
+            EntityModel<SheepRenderState> entityModel = state.isBaby ? this.babyModel : this.model;
+            coloredCutoutModelCopyLayerRender(entityModel, TEXTURE, matrices, queue, light, state, state.getWoolColor(), 0);
         }
     }
 }
